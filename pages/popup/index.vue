0130c405361f74dad8c58ed6d0bf238bfc672304
Isamu <support@isamu.ninja>
2020-10-17 12:12:41 -0300

    refresh v1

--- pages/popup/index.vue
+++ pages/popup/index.vue
@@ -1,11 +1,30 @@
 <template>
-  <div>popup</div>
+  <div style="width: 350px; height: 400px">
+    <vs-card type="4">
+      <template #title>
+        <h3>Pot with a plant</h3>
+      </template>
+      <template #img>
+        <img src="/assets/img/foto2.jpg" />
+      </template>
+      <template #text>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+      </template>
+      <template #interactions>
+        <vs-button danger icon>
+          <i class="bx bx-heart"></i>
+        </vs-button>
+        <vs-button class="btn-chat" shadow primary>
+          <i class="bx bx-chat"></i>
+          <span class="span"> 54 </span>
+        </vs-button>
+      </template>
+    </vs-card>
+  </div>
 </template>
 
 <script>
 export default {
-  mounted() {
-    console.log('popup')
-  },
+  mounted() {},
 }
 </script>
